ec41412276ebf534fbd87a611de948cf4023fbd3
Guilherme Henrique <guilherhenri12@gmail.com>
2020-05-15 11:30:43 -0300

    Feature: Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
-)
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
 
-export default App;
+    <GlobalStyle />
+  </>
+  )
+
+  export default App;
