f925a83bffdba1838a240111d7e90d23a2bcc71b
Guilherme Henrique <guilherhenri12@gmail.com>
2020-05-15 11:57:31 -0300

    Feature: Create Dashboard page and add styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositorios no Github</Title>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer"/>
+      <Title>Explore repositorios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+
+        <button>Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/36506176?s=460&u=ba466d956830f22733dbbf96cfd76033fdb5124d&v=4"
+            alt="Guilherme Henrique"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/36506176?s=460&u=ba466d956830f22733dbbf96cfd76033fdb5124d&v=4"
+            alt="Guilherme Henrique"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/36506176?s=460&u=ba466d956830f22733dbbf96cfd76033fdb5124d&v=4"
+            alt="Guilherme Henrique"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard;
